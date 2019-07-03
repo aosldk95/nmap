13360fd1d147899c8a740a54725e220c51eb022e
강병안 <aosldk95@naver.com>
2019-07-03 14:09:53 +0900

    marker

--- app/src/main/java/com/kba/nmap/MainActivity.java
+++ app/src/main/java/com/kba/nmap/MainActivity.java
@@ -1,14 +1,44 @@
 package com.kba.nmap;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+
+
     }
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        LatLng location = new LatLng(35.94528, 126.682167);
+        CameraPosition cameraPosition = new CameraPosition(location, 14);
+        naverMap.setCameraPosition(cameraPosition);
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(35.94528, 126.682167));
+        marker.setMap(naverMap);
+
+    }
+
 }
