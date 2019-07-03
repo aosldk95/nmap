888bb617e62224552d689449fd7f1c47e8d785a2
강병안 <aosldk95@naver.com>
2019-07-03 14:49:20 +0900

    add marker

--- app/src/main/java/com/kba/nmap/MainActivity.java
+++ app/src/main/java/com/kba/nmap/MainActivity.java
@@ -11,7 +11,10 @@ import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.ArrowheadPathOverlay;
 import com.naver.maps.map.overlay.Marker;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -33,12 +36,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         LatLng location = new LatLng(35.94528, 126.682167);
-        CameraPosition cameraPosition = new CameraPosition(location, 14);
+        CameraPosition cameraPosition = new CameraPosition(location, 8);
         naverMap.setCameraPosition(cameraPosition);
         Marker marker = new Marker();
         marker.setPosition(new LatLng(35.94528, 126.682167));
         marker.setMap(naverMap);
-
+        Marker marker2 = new Marker();
+        marker2.setPosition(new LatLng(35.969389, 126.957333));
+        marker2.setMap(naverMap);
+        Marker marker3 = new Marker();
+        marker3.setPosition(new LatLng(35.983, 126.717));
+        marker3.setMap(naverMap);
+        Marker marker4 = new Marker();
+        marker4.setPosition(new LatLng(35.846833, 127.129361));
+        marker4.setMap(naverMap);
+        ArrowheadPathOverlay arrowheadPath = new ArrowheadPathOverlay();
+        arrowheadPath.setCoords(Arrays.asList(
+                new LatLng(35.94528, 126.682167),
+                new LatLng(35.983, 126.717),
+                new LatLng(35.969389, 126.957333),
+                new LatLng(35.846833, 127.129361)
+        ));
+        arrowheadPath.setMap(naverMap);
+        
     }
 
 }
