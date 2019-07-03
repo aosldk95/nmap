74f543a2dff54d14e587ce3caea7ec4d7368a322
강병안 <aosldk95@naver.com>
2019-07-03 15:09:51 +0900

    infowindow

--- app/src/main/java/com/kba/nmap/MainActivity.java
+++ app/src/main/java/com/kba/nmap/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.naver.maps.geometry.LatLng;
@@ -12,6 +13,7 @@ import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.ArrowheadPathOverlay;
+import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
 
 import java.util.Arrays;
@@ -41,15 +43,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Marker marker = new Marker();
         marker.setPosition(new LatLng(35.94528, 126.682167));
         marker.setMap(naverMap);
+        Marker marker1 = new Marker();
+        marker1.setPosition(new LatLng(35.969389, 126.957333));
+        marker1.setMap(naverMap);
         Marker marker2 = new Marker();
-        marker2.setPosition(new LatLng(35.969389, 126.957333));
+        marker2.setPosition(new LatLng(35.983, 126.717));
         marker2.setMap(naverMap);
         Marker marker3 = new Marker();
-        marker3.setPosition(new LatLng(35.983, 126.717));
+        marker3.setPosition(new LatLng(35.846833, 127.129361));
         marker3.setMap(naverMap);
-        Marker marker4 = new Marker();
-        marker4.setPosition(new LatLng(35.846833, 127.129361));
-        marker4.setMap(naverMap);
         ArrowheadPathOverlay arrowheadPath = new ArrowheadPathOverlay();
         arrowheadPath.setCoords(Arrays.asList(
                 new LatLng(35.94528, 126.682167),
@@ -57,8 +59,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 new LatLng(35.969389, 126.957333),
                 new LatLng(35.846833, 127.129361)
         ));
+        Context context = this;
         arrowheadPath.setMap(naverMap);
+        InfoWindow infoWindow = new InfoWindow();
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "군산대";
+            }
+        });
+        InfoWindow infoWindow1 = new InfoWindow();
+        infoWindow1.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "원광대";
+            }
+        });
+        InfoWindow infoWindow2 = new InfoWindow();
+        infoWindow2.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "군산시청";
+            }
+        });
+        InfoWindow infoWindow3 = new InfoWindow();
+        infoWindow3.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "전북대";
+            }
+        });
+        infoWindow.open(marker);
+        infoWindow1.open(marker1);
+        infoWindow2.open(marker2);
+        infoWindow3.open(marker3);
         
+
     }
 
 }
